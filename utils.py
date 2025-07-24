78b21f4a7b4f10a4c60b01b5a2e3da017ec82f4a
Tekkermester <lorinctekker@gmail.com>
2025-07-24 16:56:01 +0200

    resurce_path to utils.py

--- utils.py
+++ utils.py
@@ -127,3 +127,6 @@ def time_split(time_:str):
     return year, month, day, hour
 
 
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+    return os.path.join(base_path, relative_path)
