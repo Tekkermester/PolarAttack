65e2e721371632ff6c0b679a8e7531a0bbda4629
Tekkermester <lorinctekker@gmail.com>
2025-07-30 12:59:50 +0200

    created shoe_sport to manage them

---
+++ shoe_sport.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class Shoes(QThread):
+    def __init__(self):
+        super().__init__()
+
+class Sports(QThread):
+    def __init__(self):
+        super().__init__()
+
