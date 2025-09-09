e011a9d49b2672d46d95ab0eeb86394666e2a7ea
Tekkermester <lorinctekker@gmail.com>
2025-09-09 06:19:03 -0700

    added chromedrvier

--- attackpoint.py
+++ attackpoint.py
@@ -74,7 +74,7 @@ class Uploading(QThread):
         chrome_binary, driver_path = chromium_path()
         options = Options()
         options.binary_location = chrome_binary
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
